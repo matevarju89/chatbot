e911436f04e919e1497f1c6785554fd1612c4cfd
matevarju89 <matevarju89@gmail.com>
2021-05-30 13:21:15 +0200

    added subscription instead of query

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 let messages = [];
 const typeDefs = `
     type Message {
@@ -14,7 +14,14 @@ const typeDefs = `
     type Mutation {
         postMessage(user: String!, content: String!) : ID!
     }
+
+    type Subscription{
+      messages: [Message!]
+    }
     `;
+
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
 const resolvers = {
   Query: {
     messages: () => messages,
@@ -27,12 +34,25 @@ const resolvers = {
         user,
         content,
       });
+      subscribers.forEach((fn) => fn());
       return id;
     },
   },
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).slice(2, 15);
+        onMessagesUpdates(() => {
+          pubsub.publish(channel, { messages });
+        });
+        setTimeout(() => pubsub.publish(channel, { messages }), 0);
+        return pubsub.asyncIterator(channel);
+      },
+    },
+  },
 };
-
-const server = new GraphQLServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 server.start(({ port }) => {
   console.log(`server running on http://localhost:${port}/`);
 });
